a8682f9901ad32def1db73a0a8727d34d6392ef6
itspavit <92730827+itspavit@users.noreply.github.com>
2024-01-02 18:41:09 -0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+adjective = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+superhero = input("Superhero Name: ")
+madlib = f"computer science is so {adjective}! It is a great field of study. I like to {verb1} in my free time or I like doing {verb2} instead sometimes. Keep shining you got this {superhero}!"
+
+print(madlib)
